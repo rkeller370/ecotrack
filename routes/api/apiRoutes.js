f276f9b20d19541a25312d260668438668894485
rkeller929 <robbiek370@gmail.com>
2025-03-06 17:44:09 -0600

    Fixed route

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -7,7 +7,7 @@ const limiter = require("../../middleware/rateLimiter");
 router.get("/universities", authenticateJWT, limiter.limiter, apiController.getUniversities);
 router.post("/eval", authenticateJWT, limiter.limiter, apiController.evaluateStudent);
 router.get("/analysis", authenticateJWT, limiter.limiter, apiController.getAnalysis);
-router.get("/settings", authenticateJWT, limiter.limiter, apiController.getAnalysis);
+router.get("/settings", authenticateJWT, limiter.limiter, apiController.getSettings);
 router.post("/essay-review", authenticateJWT, limiter.limiter, apiController.reviewEssay);
 
 module.exports = router;
