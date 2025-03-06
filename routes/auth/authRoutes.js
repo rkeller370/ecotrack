b3ba707ccc1fbb16d8d74bde341e034c561b9d19
rkeller929 <robbiek370@gmail.com>
2025-03-05 18:40:53 -0600

    Test

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -5,7 +5,7 @@ const { authenticateJWT } = require("../../middleware/authMiddleware");
 const limiter = require("../../middleware/rateLimiter");
 
 router.post("/register", limiter.loginLimiter, authController.register);
-router.post("/login", authenticateJWT, limiter.loginLimiter, authController.login);
+router.post("/login", limiter.loginLimiter, authController.login);
 router.post("/logout", authenticateJWT, limiter.limiter, authController.logout);
 router.post("/refresh", limiter.limiter, authController.refreshToken);
 router.get("/verify", authenticateJWT, limiter.limiter, authController.verify);
