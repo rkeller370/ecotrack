6b4158067891f685b8bd0317f86a8c01d9d63796
rkeller929 <robbiek370@gmail.com>
2025-03-09 13:19:39 -0500

    minor fixes

--- config/db.js
+++ config/db.js
@@ -18,23 +18,22 @@ const initializeMongo = async (retries = 5, delay = 5000) => {
       const client = await mongoClient.connect();
       db = await client.db("Development");
       console.log("Connected to MongoDB");
-      return; // Successfully connected, exit the function
+      return;
     } catch (err) {
       console.error(`Failed to connect to MongoDB (attempt ${i + 1}):`, err);
       if (i === retries - 1) {
         console.error('Max retries reached. Throwing error.');
-        throw err; // If we've exhausted retries, throw the error
+        throw err;
       }
-      await new Promise((res) => setTimeout(res, delay)); // Retry after delay
+      await new Promise((res) => setTimeout(res, delay));
     }
   }
 };
 
-// Ensure the DB is initialized before returning it
 const getDb = async () => {
   if (!db) {
     console.log("Initializing MongoDB...");
-    await initializeMongo(); // Make sure MongoDB is initialized first
+    await initializeMongo();
   }
   return db;
 };
